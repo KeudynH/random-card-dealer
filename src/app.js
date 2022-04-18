b75591b7dc3877be53b53d3eaa1b418f1a20f3b3
KeudynH <keudyn07@gmail.com>
2022-04-18 23:18:54 +0000

    code

--- src/app.js
+++ src/app.js
@@ -2,10 +2,44 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector(".number").innerHTML = generadorDeNumeros();
+  let suit = generadorDeSuits();
+  document.querySelector(".topSuit").innerHTML = suit;
+  document.querySelector(".bottomSuit").innerHTML = suit;
+  document.querySelector(".card").classList.add(generadorDeClases(suit));
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generadorDeNumeros = () => {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indiceN = ~~(Math.random() * numbers.length);
+  return numbers[indiceN];
+};
+
+let generadorDeSuits = () => {
+  let suits = ["♠", "♣", "♥", "♦"];
+  let indiceS = ~~(Math.random() * suits.length);
+  return suits[indiceS];
+};
+
+let generadorDeClases = suit => {
+  if (suit === "♥" || suit === "♦") {
+    document.querySelector(".card").classList.add("red");
+  } else {
+    document.querySelector(".card").classList.add("black");
+  }
 };
